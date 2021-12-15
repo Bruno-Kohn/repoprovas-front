0bcb53f65566088ba816479bf87d7ccc9852596b
Bruno-Kohn <brunokohn1993@gmail.com>
2021-12-15 14:10:27 -0300

    docs: initial documentation

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/reset.css';
+
+export default function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+    </>
+  );
+}
