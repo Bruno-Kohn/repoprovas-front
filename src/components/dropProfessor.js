eddce873ad11d11b939eab279a7a47545d7479be
Bruno-Kohn <brunokohn1993@gmail.com>
2021-12-18 23:55:28 -0300

    feat: getting semester

--- src/components/dropProfessor.js
+++ src/components/dropProfessor.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 export default function DropProfessor() {
   const [professorInfo, setProfessorInfo] = useState([]);
   const professor = professorInfo.map((i) => i.name);
-  console.log(professorInfo);
 
   useEffect(
     () => {
--- src/components/dropSubject.js
+++ src/components/dropSubject.js
@@ -1,15 +1,27 @@
 import OptionSubject from './optionSubject';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function DropSubject() {
-  const subject = [
-    'HTML',
-    'CSS',
-    'JAVASCRIPT',
-    'REACT',
-    'NODEJS',
-    'POSTGRESQL',
-    'TYPESCRIPT'
-  ];
+  const [subjectReq, setSubjectReq] = useState([]);
+  console.log(subjectReq);
+
+  const subject = subjectReq.map((i) => i.name).sort();
+
+  useEffect(
+    () => {
+      const request = axios.get(`http://localhost:4000/semester/subjects`);
+
+      request.then((response) => {
+        setSubjectReq(response.data);
+      });
+      request.catch((error) => {
+        console.log(error);
+      });
+    },
+    //eslint-disable-next-line
+    []
+  );
 
   return (
     <>
